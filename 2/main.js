1a67ec6745967c1613e810e621176ddc71fcf8e9
Shaun Springer <shaun.springer@gmail.com>
2013-09-19 23:50:18 -0400

    [2] Added second kata, first attempt

---
+++ 2/main.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var readline = require('readline');
+var child = require('child_process')
+
+var TARGET_LENGTH = 6;
+var wordMap = {};
+
+readline.createInterface({
+  input: fs.createReadStream('dict.txt'),
+  terminal: false
+}).on('line', function(word){
+
+  var len = word.length;
+  if (len > TARGET_LENGTH) {
+    return;
+  }
+
+  wordMap[len] = wordMap[len] || [];
+  wordMap[len].push(word);
+
+}).on ('close', function (){
+  findWords();
+});
+
+var handleResults = function(m, socket) {
+  console.log(m)
+};
+
+var findWords = function() {
+  var targets = wordMap[TARGET_LENGTH];
+  var len = TARGET_LENGTH / 2;
+
+  for (var i = 1; i < len; i++) {
+    var worker = child.fork('worker.js');
+    worker.on('message', handleResults);
+    worker.send({ words: [wordMap[i], wordMap[TARGET_LENGTH - i]], targets: targets });
+  }
+
+  if (len % 2 !== 0) {
+    var worker = child.fork('worker.js');
+    worker.on('message', handleResults);
+    worker.send({ words: [wordMap[len], wordMap[len]], targets: targets });
+  }
+};
---
+++ 2/worker.js
@@ -0,0 +1,29 @@
+process.on('message', function(m, socket) {
+  var l = m.words[0];
+  var r = m.words[1];
+  var t = m.targets;
+  var cache = {};
+  var results = [];
+
+  for (var i = 0; i < t.length; i++) {
+    var c = t[i];
+    for (var j = 0; j < l.length; j++) {
+      var w1 = l[j];
+      for (var k = 0; k < r.length; k++) {
+        var w2 = r[k];
+        if (cache[c] !== true) {
+          if (w1 + w2 == c) {
+            results.push([w1, w2, c]);
+            cache[c] = true;
+          } else if (w2 + w1 == c) {
+            results.push([w2, w1, c]);
+            cache[c] = true;
+          }
+        }
+      }
+    }
+  }
+
+  process.send(results);
+  process.exit();
+});
